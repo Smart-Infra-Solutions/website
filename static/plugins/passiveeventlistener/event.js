33ca0d8f01131772f4290e9aeec34dc8b3777f0a
Frederic Senchet <fred.chinchin@gmail.com>
2021-12-08 14:50:33 +0100

    Update event.js

--- static/plugins/passiveeventlistener/event.js
+++ static/plugins/passiveeventlistener/event.js
@@ -1,5 +1,20 @@
-jQuery.event.special.touchstart = {
-  setup: function( _, ns, handle ){
-    this.addEventListener("touchstart", handle, { passive: true });
-  }
-};
+
+$(function(){
+	// ---- this would be the plugin ----
+	// special event for non-preventDefault
+  jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ){
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  };
+	// -----------------------------------
+
+	// fix box with junk
+	$(".box").html(new Array(50).join("all contenty<br/>"));
+
+	// set up a touchevent for the box
+	$(".box").on("touchstart.noPreventDefault", function(){
+		console.log(event.type);
+		event.preventDefault();
+	});
+});
